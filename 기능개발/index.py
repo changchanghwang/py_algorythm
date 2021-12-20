687353e3e1dcc5acdbfafa221d0006337b44346d
CH_Hwang <hch950627@naver.com>
2021-12-20 09:46:40 +0900

    [add] 기능개발

--- 기능개발/index.py
+++ 기능개발/index.py
@@ -1,16 +1,19 @@
 from collections import deque
 
-progresses = [93,30,55]
-speeds = [1,30,5]
+progresses = [40, 93, 30, 55, 60, 65]
+speeds =  [60, 1, 30, 5, 10, 7]
 
 def solution(progresses, speeds):
   answer = []
+  speeds = deque(speeds)
   p = deque([x+y for x,y in zip(progresses,speeds)])
   while p:
+    print(p)
     count = 0
     if p[0] >= 100:
-      while p[0] >= 100:
+      while p and p[0] >= 100:
         p.popleft()
+        speeds.popleft()
         count += 1
       answer.append(count)
     p = deque([x+y for x,y in zip(p,speeds)])
